5023369040232f1bab7834fd39dd149c87b1a037
Shvec Sergei <fejanin@mail.ru>
2025-10-28 17:00:11 +0300

    28.10.25 17:00

--- в бланки заводов/ПОКОМ КИ/pokom_ki/Pushkarniy_ABI/main.py
+++ в бланки заводов/ПОКОМ КИ/pokom_ki/Pushkarniy_ABI/main.py
@@ -10,6 +10,7 @@ NUM_BOXES_PER_PALLET = "J"
 
 RESULT_BRUTTO = "P"
 SUM_PALLET = "P"
+TOTAL_WEIGHT_WITH_PALLET = "P"
 
 
 # file = "test.xlsx"
@@ -27,9 +28,12 @@ for row in range(1, sh.max_row + 1):
     if type(sh[f"{RESULT_BRUTTO}{row}"].value) is str and sh[f"{RESULT_BRUTTO}{row}"].value == "ИТОГО БРУТТО":
         calc_weight = calc_weight[:-1]
         sh[f"{SU_weight_brutto}{row}"] = calc_weight
+        weight_brutto = f"{SU_weight_brutto}{row}"
     if type(sh[f"{SUM_PALLET}{row}"].value) is str and sh[f"{SUM_PALLET}{row}"].value == "Кол-во паллет":
         calc_pallets = calc_pallets[:-1] + ";0)"
         sh[f"{SU_weight_brutto}{row}"] = calc_pallets
+        count_pallet = f"{SU_weight_brutto}{row}"
+    if type(sh[f"{TOTAL_WEIGHT_WITH_PALLET}{row}"].value) is str and sh[f"{TOTAL_WEIGHT_WITH_PALLET}{row}"].value == "Вес брутто  с паллетами":
+        sh[f"{SU_weight_brutto}{row}"] = f"={weight_brutto}" + f"+{count_pallet}*25"
 
 wb.save(f"{file}")
-
